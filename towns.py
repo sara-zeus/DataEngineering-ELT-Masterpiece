1129e277b843c0edfce85891e66d416808736b51
Sara Salehi <sarasalehi74@gmail.com>
2023-10-30 19:22:10 -0600

    clean up completed

--- towns.py
+++ towns.py
@@ -17,4 +17,19 @@ def read():
     return pd.DataFrame(towns, columns=["state","town"])
 
 towns= read()
+towns.to_csv("towns.csv")
 
+#%% 
+import pandas as pd
+
+def read():
+    return pd.read_csv("towns.csv", index_col=0).assign(
+        state= lambda df: df.loc[:,"state"].str.removesuffix("[edit]"), 
+        town= lambda df: df.loc[:,"town"].str.extract(r"(.+) \(.*") 
+    )
+
+
+towns=read() 
+
+# %%
+ 
